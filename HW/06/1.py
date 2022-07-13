93ff3b5832dbfbecfbf2a066a78a945c633e78a6
Reza Amin <86108223+mrisis@users.noreply.github.com>
2022-07-13 22:04:22 +0430

    Add docstring

--- HW/06/1.py
+++ HW/06/1.py
@@ -1,8 +1,24 @@
 class Matrix:
+    """ 
+    Matrix class which methods like:
+    get_matrix
+    getElement
+    setElement
+    getTranspose
+    getMultiply 
+    transpose
+    has it 
+    """
     def __init__(self, n, m):
         self.matrix = self.get_matrix(n, m)
+        """
+        This function is for initialization
+        """
 
     def get_matrix(self, n, m):
+        """
+        
+        """
         num = 1
         matrix = [[None for j in range(m)] for i in range(n)]
         for i in range(len(matrix)):
