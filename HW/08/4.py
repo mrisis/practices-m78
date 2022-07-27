384026e6c48a9d2e064b9c1fab07ae655d3c4562
rezaamin <rezaamin8889@gmail.com>
2022-07-27 21:00:16 +0430

    fixing bugs

--- HW/08/4.py
+++ HW/08/4.py
@@ -5,17 +5,20 @@ class DiscountError(Exception):
 def apply_discount(price: int, discount: float) -> int:
     # “””This Function Calculated the Final Price”””
     result = int(price * (1 - discount))
+    #1
 
     # try:
     #     assert 0 <= result <= price
     # except AssertionError as e:
     #     raise DiscountError(e)
 
+    #2
     # if 0 <= result <= price :
     #     raise DiscountError("reza")
     # else:
     #     print("ok")
 
+    #3
     try:
         assert 0 <= result <= price
     except Exception:
