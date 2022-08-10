0e9edf07047161a9703a8f181080125b295a3cb8
rezaamin <rezaamin8889@gmail.com>
2022-08-10 21:35:55 +0430

    fixing bugs

--- HW/10/3.py
+++ HW/10/3.py
@@ -1,22 +1,53 @@
-
-import  functools
+import functools
 import time
+from functools import cache
+import logging
+
+logging.basicConfig()
+_logging_level = logging.DEBUG
+_logger = logging.getLogger(__name__)
+_logger.setLevel(_logging_level)
+
+
+# __________________________
 
 def process_timer(func):
     @functools.wraps(func)
     def wapper(n):
-        start_time= time.time()
+        start_time = time.time()
         func(n)
-        end_time=time.time()
+        end_time = time.time()
         print(f"time : {end_time - start_time}")
 
     return wapper
 
 
-#_____________________
-from functools import wraps
-from functools import cache
-import time
+# _______________
+def cache1(func):
+    caches = {}
+
+    def wrapper(*args):
+
+        signature = (func, args)
+
+        _logger.debug("using singnature:", signature)
+        if signature in caches:
+            _logger.debug("retrieving  from cache :", signature)
+            result = caches[signature]
+        else:
+            _logger.debug("calculating :%s", signature)
+            result = func(*args)
+            _logger.debug("caching : %s", signature)
+            caches[signature] = result
+        return result
+
+    return wrapper
+
+
+# _____________________
+# from functools import wraps
+
+# import time
 
 
 # def timeit(func):
@@ -32,55 +63,50 @@ import time
 #
 #     return timeit_wrapper
 
-#________________________
+# ________________________
 
 
 # @process_timer
 # @timeit
 # @cache
 def recur_factorial(n):
-
     if n == 1:
         return n
     else:
         return n * recur_factorial(n - 1)
 
+
 @process_timer
-@cache
+# @cache
+@cache1
 def fac(n):
     for i in range(n):
         recur_factorial(n)
 
 
-
-
-
-
-@process_timer
+# @process_timer
 # @timeit
 # @cache
 def recur_fibo(n):
     if n <= 1:
         return n
     else:
-        return (recur_fibo(n - 1) + recur_fibo(n - 2))
+        return recur_fibo(n - 1) + recur_fibo(n - 2)
 
 
 @process_timer
-@cache
+# @cache
+@cache1
 def fib(n):
     for i in range(n):
         recur_fibo(i)
 
 
-
-
-#_________________________________
+# _________________________________
 # start_time=time.time()
 # print(recur_factorial(9))
 # end_time=time.time()
 # print(f"time : {end_time - start_time}")
-
 
 
 #
@@ -88,9 +114,9 @@ def fib(n):
 # print(recur_fibo(20))
 # end_time=time.time()
 # print(f"time : {end_time - start_time}")
-#_________________________________________
+# _________________________________________
 
 
 # print(recur_factorial(90))
-# fib(20)
+# fib(30)
 fac(400)
